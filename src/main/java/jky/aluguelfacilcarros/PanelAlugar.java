8e2c52f51dd25f50e8f87cf2f399c268590df783
Juliano Almeida <88514570+Juliano-silva@users.noreply.github.com>
2025-05-07 09:20:09 -0300

    Header

--- src/main/java/jky/aluguelfacilcarros/PanelAlugar.java
+++ src/main/java/jky/aluguelfacilcarros/PanelAlugar.java
@@ -1,20 +1,64 @@
 package jky.aluguelfacilcarros;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PanelAlugar extends JFrame {
     private JPanel AlugarForm;
     private JList list1;
+    private JPanel rotasGetVeiculos;
+    private JPanel rotasAddVeiculos;
+    private JButton button1;
+    private JPanel rotasAddUser;
+    private JTextField textField1;
+    private JButton button2;
+    private JPanel Header;
+    private JButton AddVeiculosBtn;
+    private JButton GetVeiculosBtn;
+    private JButton AddUSerBtn;
+
+    public void Header(){
+        rotasGetVeiculos.setVisible(false);
+        rotasAddVeiculos.setVisible(false);
+        rotasAddUser.setVisible(false);
+
+        AddVeiculosBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                rotasAddVeiculos.setVisible(true);
+                rotasGetVeiculos.setVisible(false);
+                rotasAddUser.setVisible(false);
+            }
+        });
+
+        GetVeiculosBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                rotasAddVeiculos.setVisible(false);
+                rotasGetVeiculos.setVisible(true);
+                rotasAddUser.setVisible(false);
+            }
+        });
+
+        AddUSerBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                rotasAddVeiculos.setVisible(false);
+                rotasGetVeiculos.setVisible(false);
+                rotasAddUser.setVisible(true);
+            }
+        });
+    }
 
     public PanelAlugar(){
         setContentPane(AlugarForm);
         setTitle("Aluguel Facil Carros");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500, 500);
+        setSize(800, 600);
         String listas[] = {"Subaru muito pica","C3","Robt CL"};
 
         list1.setListData(listas);
 
+        Header();
         setVisible(true);
     }
 
