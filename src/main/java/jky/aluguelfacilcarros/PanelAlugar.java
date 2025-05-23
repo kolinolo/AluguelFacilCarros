262ada25d5006de29b1e5d510e96bb0ec95e63ad
Juliano Almeida <88514570+Juliano-silva@users.noreply.github.com>
2025-05-23 10:25:46 -0300

    Commit Sério 😑: Melhorei o Java Swing

--- src/main/java/jky/aluguelfacilcarros/PanelAlugar.java
+++ src/main/java/jky/aluguelfacilcarros/PanelAlugar.java
@@ -9,118 +9,150 @@ import java.util.ArrayList;
 
 public class PanelAlugar extends JFrame {
     private JPanel AlugarForm;
+    // Header
     private JList list1;
     private JPanel rotasGetVeiculos;
     private JPanel rotasAddVeiculos;
     private JPanel rotasAddUser;
     private JPanel Header;
-    private JButton AddVeiculosBtn;
-    private JButton GetVeiculosBtn;
-    private JButton AddUSerBtn;
+    private JButton LinkUserAdd;
+    private JButton LinkAddVeiculo;
+    private JButton LinkGetVeiculo;
+    // Adicionar User
     private JTextField CPFTextField;
     private JTextField nomeTextField;
     private JTextField dataDeNascimentoTextField;
     private JButton adicionarUsuárioButton;
     private JLabel UserIco;
+    // Adicionar Veiculo
     private JTextField placaTextField;
     private JTextField modeloTextField;
     private JTextField corDeCarroTextField;
     private JTextField dataDaCriaçãoTextField;
     private JTextField preçoDaDiáriaTextField;
     private JButton adicionarCarroButton;
+    // Listar Veiculo
     private JButton removerButton;
     private JButton ALUGARButton;
     private JButton devolverButton;
     private JComboBox comboBox1;
     private JButton FILTRARButton;
+    private JLabel listVeiculo;
 
-    public void Header(){
+    // Função dos Botões do Header
+    public void Header(JButton LinkHeader){
         rotasGetVeiculos.setVisible(false);
         rotasAddVeiculos.setVisible(true);
         rotasAddUser.setVisible(false);
 
-        AddVeiculosBtn.addActionListener(new ActionListener() {
+        LinkHeader.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                rotasAddVeiculos.setVisible(true);
                 rotasGetVeiculos.setVisible(false);
-                rotasAddUser.setVisible(false);
-            }
-        });
-
-        ImageIcon image = new ImageIcon("C:/Users/ronal/OneDrive/Imagens/Programação/Java/AluguelFacilCarros/assets/Desconhecido.jpg");
-
-        UserIco.setIcon(image);
-        GetVeiculosBtn.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
                 rotasAddVeiculos.setVisible(false);
-                rotasGetVeiculos.setVisible(true);
                 rotasAddUser.setVisible(false);
-            }
-        });
 
-        AddUSerBtn.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                rotasAddVeiculos.setVisible(false);
-                rotasGetVeiculos.setVisible(false);
-                rotasAddUser.setVisible(true);
+                if(LinkHeader.getText().equals("Adicionar Veiculos")){
+                    rotasAddVeiculos.setVisible(true);
+                } else if (LinkHeader.getText().equals("Adicionar User")){
+                    rotasAddUser.setVisible(true);
+                }else if(LinkHeader.getText().equals("Lista de Veiculos")){
+                    rotasGetVeiculos.setVisible(true);
+                }
+
             }
         });
     }
 
-    public static void PadronInput(JTextField input) {
-        Border border = new LineBorder(Color.white, 1, true);
+    // Função para Customizar o Input e Gerar o Placeholder
+    public void Input_Padronizado(JTextField input,String placeholder){
+        Border border = new LineBorder(Color.white,2,true);
         input.setBorder(border);
+        input.setText(placeholder);
 
-        if(input.getText().isEmpty()){
-            System.out.println("Vazio");
-        }
+        input.addFocusListener(new FocusAdapter() {
+            // Se O input estiver sendo Usado o Placeholder Desaparece
+            @Override
+            public void focusGained(FocusEvent e) {
+                if(input.getText().equals(placeholder)){
+                    input.setText("");
+                }
+            }
+
+            // Se O input não estiver sendo Usado e Estiver Vazio o Placeholder Aparece
+            @Override
+            public void focusLost(FocusEvent e) {
+                if(input.getText().isEmpty()){
+                    input.setText(placeholder);
+                }
+            }
+        });
     }
 
-    public void ModificarInputs(){
-        PadronInput(placaTextField);
-        PadronInput(modeloTextField);
-        PadronInput(corDeCarroTextField);
-        PadronInput(dataDaCriaçãoTextField);
-        PadronInput(preçoDaDiáriaTextField);
+    // Alerta Função
+    public static void messageAlert(String text,String title){
+        JOptionPane optionPane = new JOptionPane(text, JOptionPane.INFORMATION_MESSAGE);
+        JDialog dialog = optionPane.createDialog(title);
+        dialog.setAlwaysOnTop(true);
+        dialog.setVisible(true);
     }
 
-    public PanelAlugar(){
+    public PanelAlugar(String titulo,int width,int height){
         setContentPane(AlugarForm);
-        setTitle("Aluguel Facil Carros");
+        setTitle(titulo);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(1200, 800);
+        setSize(width, height);
+
+        // A Janela Vai ficar em cima de todas as outras
         setLocationRelativeTo(null);
         setAlwaysOnTop(true);
-        ModificarInputs();
         // Criação de uma lista dinâmica
         ArrayList<String> list = new ArrayList<>();
 
+        // Headers
+        Header(LinkGetVeiculo);
+        Header(LinkAddVeiculo);
+        Header(LinkUserAdd);
+
+        // Input Modificar
+        Input_Padronizado(placaTextField,"Placa do Carro:");
+        Input_Padronizado(modeloTextField,"Modelo do Carro:");
+        Input_Padronizado(corDeCarroTextField,"Cor do Carro:");
+        Input_Padronizado(dataDaCriaçãoTextField,"Data do Carro:");
+        Input_Padronizado(preçoDaDiáriaTextField,"Preço da Diária do Carro:");
+
+        // Image do AddUser
+        ImageIcon image = new ImageIcon("C:/Users/ronal/OneDrive/Imagens/Programação/Java/AluguelFacilCarros/assets/Desconhecido.jpg");
+        UserIco.setIcon(image);
+
+        adicionarCarroButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                messageAlert("Carro adicionado com sucesso!","Carro adicionado");
+            }
+        });
+
+        // Sqlite combinado com Java Swing
         ConexaoDB con = new ConexaoDB(); // inicia o OBJ de conexão com o DB
 
         // Adicionando itens à lista
         for (int i = 0; i < con.Select("modelo").size(); i++){
             list.add(con.Select("modelo").get(i) + " - " + con.Select("placa").get(i));
         }
+        listVeiculo.setText("Lista de Veiculos Quantidade " + list.size());
 
-        comboBox1.addItem("Carro");
-        comboBox1.addItem("Moto");
-        comboBox1.addItem("Barco");
-        comboBox1.addItem("Avião");
-        comboBox1.addItem("Trem");
-        comboBox1.addItem("Bicicleta");
-        comboBox1.addItem("Camelo");
         // Convertendo a lista para um array
         String[] lista = new String[list.size()];
         lista = list.toArray(lista);
 
         list1.setListData(lista);
 
-        Header();
+        comboBox1.addItem("Modelo");
+        comboBox1.addItem("Fabricante");
+        comboBox1.addItem("Preço do Carro");
         setVisible(true);
     }
 
     public static void main(String[] args) {
-        new PanelAlugar();
+        new PanelAlugar("Aluguel Facil Carros",1500,750);
     }
 
 }
